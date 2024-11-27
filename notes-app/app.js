d7940343ff5d962c5e4bd59e05b3fa2d4f3730e2
CarlusInnocent <carlusinnocentainembabazi@gmail.com>
2024-11-28 00:22:08 +0300

    Dealing with the yargs package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,40 @@
-const fs = require("fs")
+import yargs from "yargs";
+import { hideBin } from 'yargs/helpers';
 
-fs.writeFileSync("notes.txt", "This Is My First Line")
+//configure yargs command
+yargs(hideBin(process.argv))
+    .command({
+        command: "add",
+        describe: "This is an add command",
+        builder:{
+            title:{
+                describe: "Note Title",
+                demandOption: true,
+                type: "string"
+            },
+            body:{
+                describe: "Note Body",
+                demandOption: true,
+                type: "string"
+            }
+        },
+        handler:(argv)=>{
+            console.log(`The note with title > ${argv.title} and body > ${argv.body} is added`)
+        }
+    })
+    .command({
+        command: "remove",
+        describe: "This is a remove command",
+        builder: {
+            title:{
+                describe: "Note Title",
+                demandOption: true,
+                type: "string"
+            }
+        },
+        handler:(argv)=>{
+            console.log(`The title to be removed is ${argv.title}`)
+        }
+    })
+    // list command and the read command
+    .parse();
